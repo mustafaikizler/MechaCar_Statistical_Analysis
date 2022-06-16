9603dfc0726413c1667780466d512245100cf9fe
mustafaikizler <98247252+mustafaikizler@users.noreply.github.com>
2022-06-16 14:37:59 -0400

    deliverable 1

---
+++ MechaCarChallenge.RScript.R
@@ -0,0 +1,3 @@
+Mechacar <- read.csv(file="MechaCar_mpg.csv", check.names = F, stringsAsFactors = F)
+lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = Mechacar)
+summary(lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = Mechacar))
