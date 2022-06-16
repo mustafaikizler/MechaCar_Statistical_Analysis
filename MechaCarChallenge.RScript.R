dda2b461994e5877198ca003692e688055789365
mustafaikizler <98247252+mustafaikizler@users.noreply.github.com>
2022-06-16 17:27:42 -0400

    Deliverable 2

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,3 +1,15 @@
 Mechacar <- read.csv(file="MechaCar_mpg.csv", check.names = F, stringsAsFactors = F)
 lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = Mechacar)
 summary(lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = Mechacar))
+
+#DELIVERABLE 2
+Suspension_Coil <- read.table(file="Suspension_Coil.csv", check.names = F, stringsAsFactors = F, sep = ",", header =TRUE)
+
+library(tidyverse)
+
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI), Variance= var(PSI), Median=median(PSI), SD=sd(PSI) ) #create summary table
+total_summary
+
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>%summarize(Mean=mean(PSI), Variance= var(PSI), Median=median(PSI), SD=sd(PSI))
+lot_summary
+
